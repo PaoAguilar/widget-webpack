62c965e483314363aeafcd2929fb661d1813a1a1
Paola Aguilar <paola.aguilar@goluuk.com>
2022-07-06 11:03:24 -0600

    adding global state

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,16 +1,37 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux'
+import { createSlice } from '@reduxjs/toolkit'
 
-const Button = (props) => {
-    const { increment } = props;
-    const [add, setAdd] = useState(1);
-    return (
-    <div style={{display: 'flex', justifyContent:'center'}}>
-      <div style={{background: 'red', width: '40%' }}>
-        <button onClick={() => setAdd(add + +increment)}>Increment</button>
-        <div>{add}</div>
+export const counterSlice = createSlice({
+  name: 'counter',
+  initialState: {
+    value: 0,
+  },
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    }
+  }
+});
+
+export const { increment, decrement } = counterSlice.actions
+
+
+const Button = () => {
+
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch()
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <div style={{ background: 'red', width: '40%' }}>
+        <button onClick={() => dispatch(increment())}>Increment</button>
+        <div>{count}</div>
       </div>
     </div>
-    );
+  );
 };
 
 export default Button;
--- src/index.js
+++ src/index.js
@@ -1,29 +1,91 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Button from './components/Button';
+import Button, { counterSlice, decrement } from './components/Button';
 
 let init = null;
 
+//---------------------------------------------------
+
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
+
+const store = configureStore({
+  reducer: {
+    counter: counterSlice.reducer,
+  },
+});
+
+//---------------------------------------------------
+
+
+
+
 export default {
-  config: (config) =>{
-      init = config;
+  config: (config) => {
+    init = config;
   },
   widgets: {
     myWidget: {
       new: () => {
-        console.log('HI')
         return {
           render: () => {
-            ReactDOM.render(<Button 
-                increment={init.increment}
-            />, document.querySelector(init.selector));
+            ReactDOM.render(
+              <Provider store={store}>
+                <Button />
+              </Provider>
+              , document.querySelector(init.selector));
           },
-          unmount(){
-            ReactDOM.unmountComponentAtNode(document.querySelector(init.selector)); 
+          showLocationHistory: (event) => {
+            store.dispatch(decrement());
+            console.log('event', event);
+          },
+          unmount() {
+            ReactDOM.unmountComponentAtNode(document.querySelector(init.selector));
           },
         }
       }
     }
   }
 }
+
+
+/**
+ * Reducer
+ *  - Config -> default -> realtime
+ *    - History
+ *    - RealTime
+ *  - RealTime
+ *    - shoperId
+ *    - orderId
+ *  - History
+ *    - orderId
+ *  - Orders -> htttps api -> Componentes pricipales
+ *  - Drivers -> https api
+ */
+
+/**
+ * Acceso a reduce -> state application state / widget state
+ * <WidgetApp>
+ * </WidgetApp>
+ */
+
+/**
+ * const widgetApp = (props)=>{
+ *  const config = useSelect((state)=> state.config);
+ *  if(config.type === 'order-history') return <OrderHistoryMap/>
+ *  if(config.type === 'realtime') return <RealtTimeMap/>
+ *  return <div> No type config found </dic>
+ * }
+ */
+
+/**
+ * const Realtime = ( props) =>{
+ * 
+ *  useEffect(()=> { dispatch(getRealtime())},[]);
+ * 
+ *  const driver = useSelect((state)=> state.realtime.drivers());
+ * 
+ *  return <map> --> logic -> punteros -> click cierta informacion -> ordenes tiene + la bateria
+ * }
+ */
